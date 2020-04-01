451ad33fbc8a7d9ed99ff9fc82e6fa9c2e1c1418
Andrew Parmar <aparmar@ditto.com>
2020-04-01 02:56:04 -0700

    Checkpoint. Using MHI class. Needs work.

--- code/Project_Files_2/utils.py
+++ code/Project_Files_2/utils.py
@@ -7,30 +7,30 @@ VID_DIR = "sample_dataset"
 
 
 class BinaryMotion:
-
     def __init__(self, len_img_history, threshold):
         self.n = len_img_history
         self.theta = threshold
+        self.images = None
+        self.last_image = None
+        self.binary_image = None
 
     def update(self, image):
-        if not hasattr(self, 'last_image'):
+        if self.images is None:
             h, w = image.shape
             self.images = np.zeros((h, w, self.n), dtype=np.uint8)
             self.last_image = image
-            self.binary_image = np.zeros((h, w))
         else:
-            self.images[:, :, :self.n - 1] = self.images[:, :, 1:self.n]
+            self.images[:, :, : self.n - 1] = self.images[:, :, 1 : self.n]
             self.images[:, :, -1] = self.last_image
             self.last_image = image
 
     def get_binary_image(self):
-        diff_image = cv2.absdiff(self.last_image, np.median(self.images, axis=2).astype(np.uint8))
+        diff_image = cv2.absdiff(
+            self.last_image, np.median(self.images, axis=2).astype(np.uint8)
+        )
 
         self.binary_image = np.zeros(diff_image.shape)
-
-        idx = diff_image >= self.theta
-        self.binary_image[idx] = 1
-
+        self.binary_image[diff_image >= self.theta] = 1
         self._cleanup()
 
         return self.binary_image
@@ -39,65 +39,46 @@ class BinaryMotion:
         kernel = np.ones((5, 5), np.uint8)
         self.binary_image = cv2.morphologyEx(self.binary_image, cv2.MORPH_OPEN, kernel)
 
-
-def get_binary_image(image1, image2, threshold):
-    # TODO: median filtering. See 8D-L1 lecture
-
-    diff_image = cv2.absdiff(image1, image2)
-
-    binary_image = np.zeros(diff_image.shape)
-
-    idx = diff_image >= threshold
-
-    binary_image[idx] = 1
-
-    binary_image = cleanup_image(binary_image)
-
-    return binary_image
+    def view(self):
+        cv2.namedWindow("binary_image", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("binary_image", (600, 600))
+        cv2.imshow("binary_image", self.binary_image)
+        cv2.waitKey(20)
 
 
-def make_motion_history_image_recursive(binary_sequence):
-    # Second try: Recursive reduction
-    h, w, n = binary_sequence.shape
+class MotionHistoryImage:
+    def __init__(self, len_img_history):
+        self.n = len_img_history
+        self.tau = 255 / self.n
+        self.image = None
 
-    mhi = np.zeros((h, w))
+    def update(self, motion_img):
+        if not hasattr(self, "last_binary_image"):
+            h, w = motion_img.shape
+            self.motion_images = np.zeros((h, w, self.n), dtype=np.uint8)
+        else:
+            self.motion_images[:, :, : self.n - 1] = self.motion_images[
+                :, :, 1 : self.n
+            ]
 
-    tau = 255
+        self.motion_images[:, :, -1] = motion_img
 
-    if n == 1:
-        idx = binary_sequence[:, :, 0] == 1
-        mhi[idx] = tau
-        mhi[~idx] = 0
-        return mhi
-    else:
-        idx = binary_sequence[:, :, 0] == 1
-        mhi[idx] = tau
-        mhi[~idx] = np.maximum(make_motion_history_image_recursive(binary_sequence[:, :, 1:]) - 30, 0)[~idx]
-        return mhi
+        self._make_motion_history_image()
 
+    def _make_motion_history_image(self):
+        h, w, n = self.motion_images.shape
 
-def make_motion_history_image(binary_sequence):
-    # First try: Reduce images by set weight
-    # import pdb; pdb.set_trace()
-    h, w, n = binary_sequence.shape
+        self.image = np.zeros((h, w))
 
-    mhi = np.zeros((h, w))
+        for i in range(n):
+            idx = self.motion_images[:, :, i] == 1
+            self.image[idx] = self.tau * i
 
-    tau = 255 / n
-
-    for i in range(n):
-        print(f"This is {i}th frame. Tau: {255 - tau*i}")
-        idx = binary_sequence[:, :, i] == 1
-        mhi[idx] = tau * i
-
-    return mhi
-
-
-def cleanup_image(image):
-    kernel = np.ones((5, 5), np.uint8)
-    cleaned_image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-    return cleaned_image
+    def view(self):
+        cv2.namedWindow("motion_history_image", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("motion_history_image", (600, 600))
+        cv2.imshow("motion_history_image", self.image)
+        cv2.waitKey(20)
 
 
 def video_to_image_array(filename, fps):
@@ -106,7 +87,6 @@ def video_to_image_array(filename, fps):
     input_image_gen = video_gray_frame_generator(input_video_path)
     input_image_t = input_image_gen.__next__()
 
-    h, w = input_image_t.shape
     n = 50  # 50 is best so far
     theta = 40  # 50 is best so far
     q = 10
@@ -114,35 +94,19 @@ def video_to_image_array(filename, fps):
     frame_num = 0
 
     binary_motion = BinaryMotion(n, theta)
-
-    last_q_binary_images = np.zeros((h, w, q), dtype=np.uint8)
+    motion_history_image = MotionHistoryImage(q)
 
     while input_image_t is not None:
-        binary_motion.update(input_image_t)
-
-        if frame_num % 10 == 0:
-            print("Processing fame {}".format(frame_num))
-
+        # if frame_num % 10 == 0:
+        #     print("Processing fame {}".format(frame_num))
         # if frame_num == 200:
         #     import pdb; pdb.set_trace()
 
-        binary_image = binary_motion.get_binary_image()
+        binary_motion.update(input_image_t)
+        motion_history_image.update(binary_motion.get_binary_image())
 
-        last_q_binary_images[:, :, : q - 1] = last_q_binary_images[:, :, 1:q]
-        last_q_binary_images[:, :, -1] = binary_image
-
-        # motion_history_image = make_motion_history_image(np.flip(last_q_binary_images, 2))
-        motion_history_image = make_motion_history_image(last_q_binary_images)
-
-        cv2.namedWindow("binary_image", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("binary_image", (600, 600))
-        cv2.imshow("binary_image", binary_image)
-
-        cv2.namedWindow("motion_history_image", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("motion_history_image", (600, 600))
-        cv2.imshow("motion_history_image", motion_history_image.astype(np.uint8))
-
-        cv2.waitKey(10)
+        binary_motion.view()
+        motion_history_image.view()
 
         input_image_t = input_image_gen.__next__()
 
