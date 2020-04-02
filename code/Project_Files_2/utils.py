e4e7fb3d12456d63a8be775c71ce274a630aa40f
Andrew Parmar <aparmar@ditto.com>
2020-04-01 21:10:43 -0700

    Fixed MHI rendering. Used np.uint8.

--- code/Project_Files_2/utils.py
+++ code/Project_Files_2/utils.py
@@ -25,6 +25,9 @@ class BinaryMotion:
             self.last_image = image
 
     def get_binary_image(self):
+        """
+        binary_image is of type np.float64
+        """
         diff_image = cv2.absdiff(
             self.last_image, np.median(self.images, axis=2).astype(np.uint8)
         )
@@ -50,16 +53,17 @@ class MotionHistoryImage:
     def __init__(self, len_img_history):
         self.n = len_img_history
         self.tau = 255 / self.n
-        self.image = None
+        self.motion_images = None
+        self.mhi = None
 
     def update(self, motion_img):
-        if not hasattr(self, "last_binary_image"):
+        if self.motion_images is None:
             h, w = motion_img.shape
             self.motion_images = np.zeros((h, w, self.n), dtype=np.uint8)
         else:
             self.motion_images[:, :, : self.n - 1] = self.motion_images[
                 :, :, 1 : self.n
-            ]
+            ]  # noqa
 
         self.motion_images[:, :, -1] = motion_img
 
@@ -68,16 +72,18 @@ class MotionHistoryImage:
     def _make_motion_history_image(self):
         h, w, n = self.motion_images.shape
 
-        self.image = np.zeros((h, w))
+        self.mhi = np.zeros((h, w))
 
         for i in range(n):
             idx = self.motion_images[:, :, i] == 1
-            self.image[idx] = self.tau * i
+            self.mhi[idx] = self.tau * i
+
+        self.mhi = self.mhi.astype(np.uint8)
 
     def view(self):
         cv2.namedWindow("motion_history_image", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("motion_history_image", (600, 600))
-        cv2.imshow("motion_history_image", self.image)
+        cv2.imshow("motion_history_image", self.mhi)
         cv2.waitKey(20)
 
 
