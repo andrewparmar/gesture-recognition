91da160c922917d28d9230b6a29d81e0ae447bdb
Andrew Parmar <aparmar@ditto.com>
2020-04-22 22:19:41 -0700

    Adding extra files.

---
+++ code/Project_Files_2/print_images.py
@@ -0,0 +1,7 @@
+from core import ActionVideo
+av1 = ActionVideo(1, 'walking', 'd1')
+av1.analyze_frames(save_frames=[200])
+
+av1 = ActionVideo(2, 'handwaving', 'd1')
+av1.analyze_frames(save_frames=[285])
+
